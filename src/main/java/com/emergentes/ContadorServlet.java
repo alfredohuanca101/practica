7575921dbc05c44ae08cb31602c2381155ba7446
alfredo Huanca <alenfiss@gmail.com>
2022-10-17 21:41:45 -0400

    Primer commit

---
+++ src/main/java/com/emergentes/ContadorServlet.java
@@ -0,0 +1,42 @@
+
+package com.emergentes;
+import javax.servlet.http.Cookie;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ContadorServlet", urlPatterns = {"/ContadorServlet"})
+public class ContadorServlet extends HttpServlet {
+
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        int contador =0;
+        Cookie[] cukis = request.getCookies();
+        if(cukis !=null){
+         for(Cookie c: cukis){
+          if(c.getName().equals("visitas")){
+           contador = Integer.parseInt(c.getValue());
+          }
+         }
+        }
+        contador++;
+        Cookie c = new Cookie("visitas",Integer.toString(contador));
+        c.setMaxAge(300);
+        response.addCookie(c);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("VisistanteN°" + contador);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+    
+        }
+    
+}
+
